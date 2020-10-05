5b2e12ffecdfc872e09ab488ca6abec0f45e7ded
DJTomos <71010478+DJTomos@users.noreply.github.com>
2020-10-05 13:53:09 +0200

    mgtServer

---
+++ DSC/mgtDSC/mgtDSCConfiguration.ps1
@@ -0,0 +1,53 @@
+Configuration Main
+{
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+
+    Node localhost
+    {
+        LocalConfigurationManager            
+        {            
+            DebugMode = 'All'
+            ActionAfterReboot = 'ContinueConfiguration'            
+            ConfigurationMode = 'ApplyOnly'            
+            RebootNodeIfNeeded = $true
+        }
+
+	    WindowsFeature Tools 
+        {
+            Ensure = "Present"
+            Name = "RSAT-RemoteAccess"
+            IncludeAllSubFeature = $true
+        }
+
+        WindowsFeature MoreTools 
+        {
+            Ensure = "Present"
+            Name = "RSAT-AD-PowerShell"
+            IncludeAllSubFeature = $true
+        }
+
+        WindowsFeature RSAT-ADCS-Mgmt
+        {
+            Ensure = 'Present'
+            Name = 'RSAT-ADCS-Mgmt'
+        }
+
+        WindowsFeature ADCS-Cert-Authority
+        {
+            Ensure = 'Present'
+            Name = 'ADCS-Cert-Authority'
+        }
+
+        WindowsFeature Web-Mgmt-Console
+        {
+            Ensure = 'Present'
+            Name = 'Web-Mgmt-Console'            
+		}	
+
+        WindowsFeature Telnet
+        {
+            Ensure = "Present"
+            Name = "Telnet-Client"
+        }
+    }
+}
